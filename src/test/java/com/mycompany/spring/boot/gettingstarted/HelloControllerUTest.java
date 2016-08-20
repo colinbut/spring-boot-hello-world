4001bb698c9aca48f83fff1e358a0a5741438f68
Colin But <colinbut@users.noreply.github.com>
2016-08-20 10:56:13 +0100

    add unit test for HelloController

--- src/test/java/com/mycompany/spring/boot/gettingstarted/HelloControllerUTest.java
+++ src/test/java/com/mycompany/spring/boot/gettingstarted/HelloControllerUTest.java
@@ -5,5 +5,36 @@
  */
 package com.mycompany.spring.boot.gettingstarted;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class HelloControllerUTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().string(equalTo("Hello World!")));
+    }
 }
