a23e53f005791fd358c014aaf6c114897d135636
Colin But <colinbut@users.noreply.github.com>
2016-08-20 10:50:37 +0100

    up junit version to 4

---
+++ src/test/java/com/mycompany/spring/boot/gettingstarted/HelloControllerUTest.java
@@ -0,0 +1,9 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.spring.boot.gettingstarted;
+
+public class HelloControllerUTest {
+}
